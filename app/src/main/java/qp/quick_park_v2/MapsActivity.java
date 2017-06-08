61a7ee0e8091c2b786204f39464524ac89cddeb3
8igMac <michaelshih123@gmail.com>
2017-06-08 22:09:36 +0800

    search bar

--- app/src/main/java/qp/quick_park_v2/MapsActivity.java
+++ app/src/main/java/qp/quick_park_v2/MapsActivity.java
@@ -2,11 +2,16 @@ package qp.quick_park_v2;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,12 +19,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private final static int MY_PERMISSION_FINE_LACATION = 101;
+    Button geoLocationBt; // search btn
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +37,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // search btn set up
+        geoLocationBt = (Button) findViewById(R.id.btSearch);
+        geoLocationBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText searchText = (EditText) findViewById(R.id.etLocationEntry);
+                String search = searchText.getText().toString();
+                if(search != null && !search.equals(""))
+                {
+                    List<Address> addressList = null;
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try {
+                        // get address info from geocoder
+                        addressList = geocoder.getFromLocationName(search, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // handling info got: address, latlng
+                    Address address = addressList.get(0);
+                    LatLng latlng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15));
+                }
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng bigCity = new LatLng(24.80, 120.97);
-        mMap.addMarker(new MarkerOptions().position(bigCity).title("Marker in bigCity"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bigCity,10));
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
@@ -49,6 +77,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    // ask for location permission
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
